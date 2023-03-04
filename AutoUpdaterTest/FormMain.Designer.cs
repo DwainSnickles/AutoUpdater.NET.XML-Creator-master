5e20fe19bca32d1deb2f6cf8dd02fc6a13c3ae29
Dwain Sickles <dwainsnickles@hotmail.com>
2023-03-04 10:36:40 -0600

    Update FormMain.Designer.cs

--- AutoUpdaterTest/FormMain.Designer.cs
+++ AutoUpdaterTest/FormMain.Designer.cs
@@ -7,20 +7,6 @@ namespace AutoUpdaterTest
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -30,9 +16,7 @@ namespace AutoUpdaterTest
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+          
         }
 
         #endregion
